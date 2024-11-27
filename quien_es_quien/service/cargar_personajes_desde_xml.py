f5536adf7c4e8700abd11b8f5b0af61f3aa1e046
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-11-27 20:10:56 +0100

    feat(Servicios): :sparkles: Añadir módulos cargar personaje y escoger al azar

--- quien_es_quien/service/cargar_personajes_desde_xml.py
+++ quien_es_quien/service/cargar_personajes_desde_xml.py
@@ -1,28 +1,28 @@
 import xml.etree.ElementTree as et
 import os
 
+
+def comprobar_ruta():
+    
+        archivo = os.path.join(os.path.dirname(__file__), "../model/personajes.xml")
+        return archivo
+    
+
 def cargar_personajes_desde_xml():
     
     try:
-        archivo = os.path.join(os.path.dirname(__file__), "../model/personajes.xml")
-
-        tree = et.parse(archivo)
-        raiz = tree.getroot()
-
+        archivo = comprobar_ruta()
+        arbol = et.parse(archivo)
+        raiz = arbol.getroot()
 
         personajes = []
         for nodo_personaje in raiz.findall("personaje"):
             personaje = {atributo: nodo_personaje.get(atributo) for atributo in nodo_personaje.keys()}
-
-            if "gafas" in personaje:
-                personaje["gafas"] = personaje["gafas"] == "True"
-            if "sombrero" in personaje:
-                personaje["sombrero"] = personaje["sombrero"] == "True"
             personajes.append(personaje)
-
         return personajes
     except FileNotFoundError:
         return "Archivo no encontrado"
 
 personajes_cargados = cargar_personajes_desde_xml()
-print(personajes_cargados)
+
+print(comprobar_ruta())
---
+++ quien_es_quien/service/personaje_random.py
@@ -0,0 +1,7 @@
+import random
+
+def elegir_personaje_random(lista_personajes):
+    return random.choice(lista_personajes)
+
+def elegir_nombre_personaje_random(lista_personajes):
+    return random.choice(lista_personajes)['nombre']
