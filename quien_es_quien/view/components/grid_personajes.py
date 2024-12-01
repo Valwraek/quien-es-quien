2205af8bd000185e19e6697e68cff5f85378a50e
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-12-01 22:33:55 +0100

    feat(Componentes): :sparkles: Añadir cabecera con mensaje empezar partida

--- quien_es_quien/view/components/grid_personajes.py
+++ quien_es_quien/view/components/grid_personajes.py
@@ -7,49 +7,60 @@ def grid_personajes():
     personajes = cargar_personajes_desde_xml()
     lista_nombres = [personaje['nombre'] for personaje in personajes]
 
-    return rx.grid(
-        rx.foreach(
-            lista_nombres,
-            lambda nombre: rx.cond(
-                Elegir_personaje.personaje_clicado,
-                # True
-                rx.card(
-                    rx.inset(
-                        rx.image(
-                            src="https://reflex.dev/reflex_banner.png",
-                            width="100%",
-                            height="auto",
+    return rx.box( 
+        rx.cond(
+            ~Elegir_personaje.personaje_clicado,
+            rx.center(
+                rx.heading("Elije una tarjeta para empezar la partida" ,as_="h1"),
+                
+            ), 
+        ),        
+        rx.grid(
+                rx.foreach(
+                    lista_nombres,
+                    lambda nombre: rx.cond(
+                        Elegir_personaje.personaje_clicado,
+                        # True
+                        rx.container(
+                            rx.card(
+                                rx.inset(
+                                    rx.image(
+                                        src="https://reflex.dev/reflex_banner.png",
+                                        width="100%",
+                                        height="auto",
+                                    ),
+                                    side="top",
+                                    pb="current",
+                                ),
+                                rx.text(
+                                    nombre,
+                                    text_align="center",
+                                    font_weight="bold",
+                                ),
+                            ),
                         ),
-                        side="top",
-                        pb="current",
-                    ),
-                    rx.text(
-                        nombre,
-                        text_align="center",
-                        font_weight="bold",
-                    ),
-                ),
 
-                # Si False devolver Fallback
-                rx.card(
-                    rx.inset(
-                        rx.image(
-                            src="/quien-es-quien.jpg",
-                            width="100%",
-                            height="auto",
+                        # Si False devolver Fallback
+                        rx.card(
+                            rx.inset(
+                                rx.image(
+                                    src="/quien-es-quien.jpg",
+                                    width="100%",
+                                    height="auto",
+                                ),
+                                side="top",
+                                pb="current",
+                            ),
+                            rx.text(
+                                "?",
+                                text_align="center",
+                                font_weight="bold",
+                            ),
+                            on_click=Elegir_personaje.personaje_seleccionado,
                         ),
-                        side="top",
-                        pb="current",
                     ),
-                    rx.text(
-                        "?",
-                        text_align="center",
-                        font_weight="bold",
-                    ),
-                    on_click=Elegir_personaje.personaje_seleccionado,
                 ),
-            ),
-        ),
-        gap="1rem",
-        grid_template_columns="repeat(6, 1fr)",
+                gap="1rem",
+                grid_template_columns="repeat(6, 1fr)",
+            )
     )
