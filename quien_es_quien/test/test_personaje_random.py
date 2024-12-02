879fd3df34124ec5f10b65d9350cca3f7e3eb39c
Valwraek <bardiarajabirajabi@gmail.com>
2024-12-02 14:11:46 +0100

    test(test): :white_check_mark: Arregar comprobación test_personaje_random.py

--- quien_es_quien/test/test_personaje_random.py
+++ quien_es_quien/test/test_personaje_random.py
@@ -4,10 +4,10 @@ from ..service.cargar_personajes_desde_xml import cargar_personajes_desde_xml as
 
 
 def test_elegir_personaje_random():
-    assert type(elegir_personaje_random(xml())) == dict
+    assert type(elegir_personaje_random()) == dict
 
 def test_elegir_nombre_personaje_random():
-    assert type(elegir_nombre_personaje_random(xml())) == str
+    assert type(elegir_nombre_personaje_random()) == str
 
 
 if __name__ == "__main__":
