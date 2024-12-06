f5f4ac213c7083630da11f6cf0ce71ce91f401a4
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-12-06 12:58:57 +0100

    refactor(Componentes): :recycle: Código gato_personajes que no se utiliza

--- quien_es_quien/view/components/area_texto.py
+++ quien_es_quien/view/components/area_texto.py
@@ -1,7 +1,7 @@
 import reflex as rx
 from quien_es_quien.controller.pregunta_respuesta import Interaccion
 from quien_es_quien.view.styles.tablero_personajes_estilos import estilos as cs
-from quien_es_quien.controller.gato_personajes import VivoOMuerto
+
 def area_texto() -> rx.Component:
     return (
         rx.input(
@@ -9,7 +9,6 @@ def area_texto() -> rx.Component:
             value=Interaccion.question,
             on_change=Interaccion.set_question,
             on_key_down=Interaccion.flujo_trabajo,
-            on_key_up=VivoOMuerto.estan_muriendo,
             autofocus=True,
             style=cs.input_style,
             is_required=True
