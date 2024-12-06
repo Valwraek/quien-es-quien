a679982bd01a4c2fc6fff1153b18bdb96dc88c41
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-12-06 13:07:48 +0100

    feat(Servicio): :sparkles: Añadir comprobar atributos del personaje a adivinar

--- quien_es_quien/service/comprobar_atributos_personaje.py
+++ quien_es_quien/service/comprobar_atributos_personaje.py
@@ -1,13 +1,9 @@
 from quien_es_quien.service.cargar_personajes_desde_xml import cargar_personajes_desde_xml as cargar_xml
 from quien_es_quien.service.personaje_a_adivinar import personaje as personaje_a_adivinar
-from quien_es_quien.controller.pregunta_respuesta import Interaccion
 
-def comprobar_atributos_personajes():
+def comprobar_atributos_personajes(atributo):
     try:
-        atributo = Interaccion.valor_question
-        print("comprobar:", [personaje['nombre'] for personaje in cargar_xml() if personaje[f'{atributo}'] != personaje_a_adivinar[f'{atributo}'] ])
         return [personaje['nombre'] for personaje in cargar_xml() if personaje[f'{atributo}'] != personaje_a_adivinar[f'{atributo}'] ]
     except KeyError:
-        print("NO FURRULA!")
         return []
     
