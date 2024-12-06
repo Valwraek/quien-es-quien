acab2cc6912faada32b315a91ae3cf38b357fdb7
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-12-06 13:09:54 +0100

    feat(Controlador): :sparkles: Editar variables de los personajes al preguntar atributos

--- quien_es_quien/controller/pregunta_respuesta.py
+++ quien_es_quien/controller/pregunta_respuesta.py
@@ -2,11 +2,14 @@ import reflex as rx
 from ..service.personaje_a_adivinar import preguntar_atributos
 from quien_es_quien.service.cargar_personajes_desde_xml import cargar_personajes_desde_xml as cargar_xml
 from quien_es_quien.service.personaje_a_adivinar import personaje as personaje_a_adivinar
+from quien_es_quien.service.comprobar_atributos_personaje import comprobar_atributos_personajes
 
 class Interaccion(rx.State):
 
     question: str
     chat_history: list[tuple[str, str]]
+    vivos = list = [personaje['nombre'] for personaje in cargar_xml()]
+    muertos = set = set()
 
     def flujo_trabajo(self, key: str):
 
@@ -17,12 +20,12 @@ class Interaccion(rx.State):
                 return rx.window_alert("No puedes enviar mensajes vacios ni que contengan números!")              
             else:
                 self.respuesta()
-
-    @rx.var
+                self.estan_muriendo()
+                self.question = ""
+    
     def valor_question(self):
         try:
             atributo = self.question
-            print(self.question)
             print("comprobar:", [personaje['nombre'] for personaje in cargar_xml() if personaje[f'{atributo}'] != personaje_a_adivinar[f'{atributo}'] ])
             return [personaje['nombre'] for personaje in cargar_xml() if personaje[f'{atributo}'] != personaje_a_adivinar[f'{atributo}'] ]
         except KeyError:
@@ -46,4 +49,13 @@ class Interaccion(rx.State):
             self.chat_history[-1][0],
             answer,
         )
+
+    def todos_vivos(self):
         
+        self.muertos = set()      
+        self.vivos = [personaje['nombre'] for personaje in cargar_xml()]
+    
+    def estan_muriendo(self):
+        
+        self.muertos.update(comprobar_atributos_personajes(self.question))
+        self.vivos = [personaje for personaje in self.vivos if personaje not in self.muertos]
