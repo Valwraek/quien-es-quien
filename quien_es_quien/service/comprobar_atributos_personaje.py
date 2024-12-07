ede3fe70f84ac7f4c742a94d7d2728add37c8fdb
Valwraek <bardiarajabirajabi@gmail.com>
2024-12-07 15:53:30 +0100

    fix(Servicios): :bug: Corregir valor de la variable debido a la importacion

--- quien_es_quien/service/comprobar_atributos_personaje.py
+++ quien_es_quien/service/comprobar_atributos_personaje.py
@@ -1,8 +1,9 @@
 from quien_es_quien.service.cargar_personajes_desde_xml import cargar_personajes_desde_xml as cargar_xml
-from quien_es_quien.service.personaje_a_adivinar import personaje as personaje_a_adivinar
 
 def comprobar_atributos_personajes(atributo):
+    from quien_es_quien.service.personaje_a_adivinar import personaje as personaje_a_adivinar
     try:
+        
         return [personaje['nombre'] for personaje in cargar_xml() if personaje[f'{atributo}'] != personaje_a_adivinar[f'{atributo}'] ]
     except KeyError:
         return []
