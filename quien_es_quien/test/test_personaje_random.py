65f33ddbd03da694873510aa119709a36a8610cc
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-12-25 14:01:17 +0100

    test(Test): :fire: Eliminar caso test de escoger nombre aleatorio

--- quien_es_quien/test/test_personaje_random.py
+++ quien_es_quien/test/test_personaje_random.py
@@ -6,11 +6,7 @@ from ..service.cargar_personajes_desde_xml import cargar_personajes_desde_xml as
 def test_elegir_personaje_random():
     assert type(elegir_personaje_random()) == dict
 
-def test_elegir_nombre_personaje_random():
-    assert type(elegir_nombre_personaje_random()) == str
-
 
 if __name__ == "__main__":
     test_elegir_personaje_random()
-    test_elegir_nombre_personaje_random()
     print("Todos los tests pasaron")
